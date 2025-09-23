ab57b3512881576f77afe8ba4cd46b58af935611
Samuel Piron <samuel.piron@studenti.unipd.it>
2025-09-23 10:07:59 +0200

    push

--- GutBrain-WebApp/backend/urls.py
+++ GutBrain-WebApp/backend/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("app/gutbrainkb/api/", include("api.urls")),
-    re_path(r'^app/gutbrainkb(/.*)?$', TemplateView.as_view(template_name="index.html")),
 ]
